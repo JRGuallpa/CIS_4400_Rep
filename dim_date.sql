b8cd89faf958eacb80265ada8ca73d5d1caf28bb
JRGuallpa <123701881+JRGuallpa@users.noreply.github.com>
2024-04-29 21:13:21 -0400

    Delete dim_date.sql

--- dim_date.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-SELECT 
-    ride_id,
-    started_at,
-    ended_at,
-    Month_Name AS month,
-    Day_of_Week AS day_of_week,
-    Hour AS hour 
-FROM testingproject-384800.cis4400_hw_dataset.citi_bike_table
